e8c93d57e83c1d4e6fed8498ce6bde5d6930d07b
Wall-e <you@mail.com>
2023-10-02 20:03:25 +0300

    Решение задачи 26

---
+++ Task02(26)/Program.cs
@@ -0,0 +1,24 @@
+﻿/* На вход: Число n
+На выход: количество цифр в числе */
+
+int KolNums (int num)
+{
+    if (num == 0) return 1;
+    int kol = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        kol += 1;
+    }
+    return kol;
+}
+
+int n;
+Console.Write("Введите целое число n: ");
+while(!int.TryParse(Console.ReadLine(), out n))
+    Console.Write("Ошибка ввода. Введите целое число: ");
+
+
+
+int countNum = KolNums (n);
+Console.Write($"{n} -> {countNum} ");
