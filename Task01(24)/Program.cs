73a5ee2f04fe4aaa51fec1d5ebcbb23b3b6ab09c
Wall-e <you@mail.com>
2023-10-02 19:46:01 +0300

    Решение задачи 24

---
+++ Task01(24)/Program.cs
@@ -0,0 +1,31 @@
+﻿/* На вход: Число а
+На выход: сумма от 1 до а */
+
+int SummaNumbers (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum += i;
+        }   
+    }
+    return sum;
+}
+
+int a;
+Start:
+Console.Write("Введите целое число a: ");
+while(!int.TryParse(Console.ReadLine(), out a))
+    Console.Write("Ошибка ввода. Введите целое число: ");
+
+if (a < 1)
+{
+    Console.WriteLine("Ошибка ввода");
+    goto Start;
+    //return;
+}
+
+int sNums = SummaNumbers(a);
+Console.Write($"{a} -> {sNums} ");
